ad7dff8942500577856f601bb4173ad0852cefce
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-03-03 03:26:31 +0600

    LWS

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -19,33 +19,53 @@ function LoginForm() {
       onSubmit={handleSubmit(submitForm)}
       className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
     >
-      {/* <!-- email --> */}
-      <div className="form-control">
-        <label className="auth-label" htmlFor="email">
-          Email
-        </label>
-        <input className="auth-input" name="email" type="email" id="email" />
-      </div>
-      {/* <!-- password --> */}
-      <div className="form-control">
-        <label className="auth-label" htmlFor="email">
-          Password
-        </label>
+      <Field label="Email">
         <input
-          className="auth-input"
-          name="password"
-          type="password"
-          id="password"
+          {...register('email', { required: 'Eamil ID is Required' })}
+          type="email"
+          name="email"
+          id="email"
+          className={` auth-input ${
+            errors.email ? 'border-red-500' : 'boreder-gray-200'
+          } `}
         />
-      </div>
-      {/* <!-- Submit --> */}
-      <button
-        className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
-        type="submit"
-      >
-        Login
-      </button>
+      </Field>
     </form>
   );
 }
 export default LoginForm;
+
+const demo = () => {
+  {
+    /* <!-- email --> */
+  }
+  <div className="form-control">
+    <label className="auth-label" htmlFor="email">
+      Email
+    </label>
+    <input className="auth-input" name="email" type="email" id="email" />
+  </div>;
+  {
+    /* <!-- password --> */
+  }
+  <div className="form-control">
+    <label className="auth-label" htmlFor="email">
+      Password
+    </label>
+    <input
+      className="auth-input"
+      name="password"
+      type="password"
+      id="password"
+    />
+  </div>;
+  {
+    /* <!-- Submit --> */
+  }
+  <button
+    className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+    type="submit"
+  >
+    Login
+  </button>;
+};
