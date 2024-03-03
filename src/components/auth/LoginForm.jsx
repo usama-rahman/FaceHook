4e960265b0643f8b921a84a8c00461f16f514316
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-03-03 22:05:00 +0600

    OK

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable no-extra-boolean-cast */
 /* eslint-disable no-unused-vars */
 import { useForm } from 'react-hook-form';
 
 import Field from '../common/Field';
+import { useNavigate } from 'react-router-dom';
 
 function LoginForm() {
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
@@ -12,42 +16,46 @@ function LoginForm() {
 
   const submitForm = (formData) => {
     console.log(formData);
+    navigate('/');
   };
 
   return (
     <form
-      onSubmit={handleSubmit(submitForm)}
       className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+      onSubmit={handleSubmit(submitForm)}
     >
-      <Field label="Email">
+      <Field label="Email" error={errors.email}>
         <input
-          {...register('email', { required: 'Eamil ID is Required' })}
+          {...register('email', { required: 'Email ID is Required' })}
+          className={`auth-input ${
+            !!errors.email ? 'border-red-500' : 'border-gray-200'
+          }`}
           type="email"
           name="email"
           id="email"
-          className={` auth-input ${
-            errors.email ? 'border-red-500' : 'boreder-gray-200'
-          } `}
         />
       </Field>
 
-      <Field label="Password">
+      <Field label="Password" error={errors.password}>
         <input
-          {...register('password', { required: 'Eamil ID is Required' })}
-          name="password"
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 8,
+              message: 'Your password must be at least 8 characters',
+            },
+          })}
+          className={`auth-input ${
+            !!errors.password ? 'border-red-500' : 'border-gray-200'
+          }`}
           type="password"
+          name="password"
           id="password"
-          className={` auth-input ${
-            errors.email ? 'border-red-500' : 'boreder-gray-200'
-          } `}
         />
       </Field>
 
       <Field>
-        <button
-          className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
-          type="submit"
-        >
+        <button className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90">
           Login
         </button>
       </Field>
