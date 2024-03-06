7c472ecf7736b6178f6c30dfaaa87ca75839b638
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-03-06 15:42:56 +0600

    Server Done

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,25 +1,26 @@
-import { useAuth } from "../hooks/useAuth"
+import { useAuth } from "../hooks/useAuth";
 import { Outlet, Navigate } from "react-router-dom";
 import Header from "../components/common/Header";
+
 const PrivateRoutes = () => {
   const { auth } = useAuth();
 
   return (
     <>
-        {
-            auth.user ? (
-                <main className="mx-auto max-w-[1020px] py-8">
-                    <div className="container">
-                        <Header />
-                        <Outlet />
-                    </div>
-                </main>
-            ) : (
-                <Navigate to="/login" />
-            )
-        }
+      {auth.user ? (
+        <>
+          <Header />
+          <main className="mx-auto max-w-[1020px] py-8">
+            <div className="container">
+              <Outlet />
+            </div>
+          </main>
+        </>
+      ) : (
+        <Navigate to="/login" />
+      )}
     </>
-  )
-}
+  );
+};
 
 export default PrivateRoutes;
